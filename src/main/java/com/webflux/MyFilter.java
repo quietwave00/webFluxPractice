86473fd6e0bed24106444f19c0635f28d15a2756
갸갸 <quietwave00@gmail.com>
2023-04-29 20:36:04 +0900

    feat: SSE 구현해보기

--- src/main/java/com/webflux/MyFilter.java
+++ src/main/java/com/webflux/MyFilter.java
@@ -1,11 +1,18 @@
 package com.webflux;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@RequiredArgsConstructor
 public class MyFilter implements Filter {
+
+    private final EventNotify eventNotify;
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         System.out.println("필터 실행");
@@ -26,6 +33,12 @@ public class MyFilter implements Filter {
 
         while(true) {
             try {
+                if(eventNotify.getChange()) {
+                    int lastIndex = eventNotify.getEvents().size() - 1;
+                    out.print("응답: " + eventNotify.getEvents().get(lastIndex) + "\n");
+                    out.flush();
+                    eventNotify.setChange(false);
+                }
                 Thread.sleep(1);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
--- src/main/java/com/webflux/MyFilterConfig.java
+++ src/main/java/com/webflux/MyFilterConfig.java
@@ -1,5 +1,6 @@
 package com.webflux;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,13 +8,25 @@ import org.springframework.context.annotation.Configuration;
 import javax.servlet.Filter;
 
 @Configuration
+@RequiredArgsConstructor
 public class MyFilterConfig {
+
+    private final EventNotify eventNotify;
+
 
     @Bean
     public FilterRegistrationBean<Filter> addFilter() {
         System.out.println("필터 등록됨");
-        FilterRegistrationBean<Filter> bean = new FilterRegistrationBean<>(new MyFilter());
-        bean.addUrlPatterns("/*");
+        FilterRegistrationBean<Filter> bean = new FilterRegistrationBean<>(new MyFilter(eventNotify));
+        bean.addUrlPatterns("/sse");
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<Filter> addFilter2() {
+        System.out.println("필터 등록됨");
+        FilterRegistrationBean<Filter> bean = new FilterRegistrationBean<>(new MyFilter2(eventNotify));
+        bean.addUrlPatterns("/add");
         return bean;
     }
 }
