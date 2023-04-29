e4a0e8d365f4d53109b69c2ab6bf1cdaae10e98c
갸갸 <quietwave00@gmail.com>
2023-04-29 20:14:30 +0900

    feat: Flux 구현해보기

--- src/main/java/com/webflux/MyFilter.java
+++ src/main/java/com/webflux/MyFilter.java
@@ -10,6 +10,26 @@ public class MyFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         System.out.println("필터 실행");
 
+        HttpServletResponse servletResponse = (HttpServletResponse) response;
+        servletResponse.setContentType("text/event-stream; charset=utf-8");
 
+        PrintWriter out = servletResponse.getWriter();
+        for(int i = 0; i < 5; i++) {
+            out.print("응답: " + i + "\n");
+            out.flush();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        while(true) {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
